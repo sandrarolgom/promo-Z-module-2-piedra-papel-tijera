438d0098a99256a431612494b216d4d57197067c
sandrarolgom <sandraroldan210900@gmail.com>
2024-08-16 10:58:23 +0200

    contador de puntos

--- js/main.js
+++ js/main.js
@@ -3,6 +3,14 @@
 const select = document.querySelector('.js-select');
 const button = document.querySelector('.js-button');
 const result = document.querySelector('.js-result');
+const userPoints = document.querySelector('.js-userpoints');
+const pcPoints = document.querySelector('.js-pcpoints');
+
+userPoints.innerHTML = `Jugador: 0`
+pcPoints.innerHTML = `Jugador: 0`
+
+let userCounter = 0;
+let pcCounter = 0;
 
 //funciones
 function getRandomNumber(max) {
@@ -29,14 +37,20 @@ const compare = ()=>{
     if (userOption === pcValue){
         result.innerHTML = 'EMPATE';
     } else if 
-    (userOption==='rock' && pcValue==='seasor' || userOption==='seasor' && pcValue==='paper' || 
+    (userOption==='rock' && pcValue==='seasor' || 
+    userOption==='seasor' && pcValue==='paper' || 
     userOption==='paper' && pcValue==='rock') {
-        result.innerHTML = '¡HAS GANADO!'
+        result.innerHTML = '¡HAS GANADO!';
+        userCounter += 1;
+        userPoints.innerHTML = `Jugador: ${userCounter}`;
+
     } else if 
     (userOption==='seasor' && pcValue==='rock' || 
     userOption==='paper' && pcValue==='seasor' || 
     userOption==='rock' && pcValue==='paper'){
-        result.innerHTML = '¡HAS PERDIDO!'
+        result.innerHTML = '¡HAS PERDIDO!';
+        pcCounter += 1;       
+        pcPoints.innerHTML = `Computadora: ${pcCounter}`;
     }
 }
 function handleClick(event){
